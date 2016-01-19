9ab83db248add83c2382fd6680fbe369cff933b2
franzcatch <franzcatch@hotmail.com>
2016-01-19 01:16:48 -0600

    readding

---
+++ MGP/app/areas/contact/contactViewModel.js
@@ -0,0 +1,27 @@
+﻿define(['file/fileViewModel'],
+    function (fileViewModel) {
+        var viewModel;
+
+        viewModel = function () {
+            var self = this;
+
+            var files = [
+                {
+                    text: 'MGP Support Group 2013',
+                    url: 'app/files/contact/MGP%20Support%20Group%202013.pdf'
+                }
+            ];
+
+            self.fileVm = new fileViewModel(files);
+
+            $.get('app/areas/file/fileView.htm', function (data) {
+                var element = $(".contactContainer");
+                element.html(data);
+                ko.applyBindingsToDescendants(self.fileVm, element[0]);
+            });
+
+            return self;
+        };
+
+        return viewModel;
+    });
---
+++ MGP/app/lib/init.js
@@ -0,0 +1,19 @@
+﻿define([],
+	function () {
+	    'use strict';
+	    requirejs.config({
+	        baseUrl: 'App/areas'
+	    });
+
+	    //now begin page ...stuff
+	    require(['shell/shellViewModel'],
+				function (shellViewModel) {
+				    var vm = new shellViewModel();
+				    
+				    pager.extendWithPage(vm);
+
+				    ko.applyBindings(vm);
+
+				    pager.start();
+				});
+	});
